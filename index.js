64a8c82ccb3de30ffd6a862e591eafca2b103c3f
mhigh <MackenzieHigh2995@GMail.com>
2023-09-05 03:35:33 -0400

    initial prototype

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
 
+var map = null;
+var polyline = null;
+
 async function main ()
 {
     console.log("loading");
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    map = L.map('map').setView([51.505, -0.09], 13);
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Station",
-        "amenity": "Train Station",
-        "popupContent": "This is where the chunnel starts!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [51.505, -0.09]
-    }
-};
+    var geojsonFeature = {
+        "type": "Feature",
+        "properties": {
+            "name": "Station",
+            "amenity": "Train Station",
+            "popupContent": "This is where the chunnel starts!"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [51.505, -0.09]
+        }
+    };
+        
+        L.geoJSON(geojsonFeature).addTo(map);
     
-    L.geoJSON(geojsonFeature).addTo(map);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'Â© OpenStreetMap'}).addTo(map);    
-
-var popup = L.popup()
-    .setLatLng([51.505, -0.09])
-    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
-    .openOn(map);
-
-// create a red polyline from an array of LatLng points
-var latlngs = [
-    [45.51, -122.68],
-    [37.77, -122.43],
-    [34.04, -118.2]
-];
-
-var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-
-// zoom the map to the polyline
-map.fitBounds(polyline.getBounds());
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'}).addTo(map);    
+    
+    // create a red polyline from an array of LatLng points
+    var latlngs = [
+        [45.51, -122.68],
+        [37.77, -122.43],
+        [34.04, -118.2]
+    ];
+    
+    polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+    
+    // zoom the map to the polyline
+    map.fitBounds(polyline.getBounds());
 
     console.log("loaded");
 }
 
+function resize ()
+{
+    console.log("resize");
+    map.invalidateSize();
+    map.fitBounds(polyline.getBounds());
+}
+
 window.addEventListener("load", main);
+
+UIkit.util.on("#render_map", "show", resize);
+
